b800754f7564bba88f1414d693d14287cad2751d
paulokatho <pkatho@gmail.com>
2017-01-02 16:47:03 -0200

    Ordenando a pesquisa por sku ou por nome

--- src/main/java/com/katho/beebee/controller/page/PageWrapper.java
+++ src/main/java/com/katho/beebee/controller/page/PageWrapper.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -56,4 +58,48 @@ public class PageWrapper<T> {
 	public String urlParaPagina(int pagina) {
 		return uriBuilder.replaceQueryParam("page", pagina).build(true).encode().toUriString();
 	}
+	
+	public String urlOrdenada(String propriedade) {
+		UriComponentsBuilder uriBuilderOrder = UriComponentsBuilder
+				.fromUriString(uriBuilder.build(true).encode().toUriString());
+		
+		String valorSort = String .format("%s,%s", propriedade, inverterDirecao(propriedade));
+		
+		return uriBuilderOrder.replaceQueryParam("sort", valorSort).build(true).encode().toUriString();
+	}
+
+	private String inverterDirecao(String propriedade) {
+		
+		String direcao = "asc";
+		
+		Order order = page.getSort() != null ? page.getSort().getOrderFor(propriedade) : null;
+		if (order != null) {
+			direcao = Sort.Direction.ASC.equals(order.getDirection()) ? "desc" : "asc";
+		}
+		
+		return direcao;
+	}
+	
+	public boolean descendente(String propriedade) {
+		return inverterDirecao(propriedade).equals("asc");
+	}
+	
+	/***
+	 * 
+	 * @param propriedade
+	 * @return se a página está ordenada ou não
+	 * 
+	 * 	Na pagina de pesquisa se não tem nenhuma ordenação ou é a primeira vez que a pesquisa é realizada
+	 * 		não tem nenhuma setinha, pra cima ou pra baixo, mas caso for clicado na ordenação por sku ou nome,
+	 * 		a ordenação é realizada.
+	 */
+	public boolean ordenada(String propriedade) {
+		Order order = page.getSort() != null ? page.getSort().getOrderFor(propriedade) : null; //Order também é para descobrir se a página está ordenada.
+		
+		if (order == null) { //se não tem ordem nenhuma ele já sai do método
+			return false;
+		}
+		return page.getSort().getOrderFor(propriedade) != null ? true : false;
+	}
+	
 }
